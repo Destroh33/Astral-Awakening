9314cdf54d7730501dd2d7b60e662c08020f4118
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2025-07-23 16:43:49 -0700

    dialogue UI stuff made + TMP imported

--- Assets/Scripts/DialogueZone.cs
+++ Assets/Scripts/DialogueZone.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class DialogueZone : MonoBehaviour
 {
+    [Header("Put this on the box collider of the zone you want to enter to trigger dialogue")]
+    [Header("--------------------")]
+    [Header("Whether this triggers automatically or needs to be interacted with")]
+    public bool needsInteraction;
+    public string textToDisplay;
+    public Sprite speakerIcon;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
