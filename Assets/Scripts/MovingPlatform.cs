e45a68c32660e257785d7d35fc5786f2d39f087b
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2025-07-24 15:16:22 -0700

    rotating platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
@@ -11,17 +13,17 @@ public class MovingPlatform : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (moveAutomatically) 
+        if (moveAutomatically)
         {
             Vector2.Lerp(transform.position, movePositions[nextPos].position, moveSpeed * Time.deltaTime);
         }
-        if (Vector2.Distance(transform.position, movePositions[nextPos].position) < detectRad) 
+        if (Vector2.Distance(transform.position, movePositions[nextPos].position) < detectRad)
         {
             nextPos++;
             if (nextPos > movePositions.Length) nextPos = 0;
@@ -30,7 +32,7 @@ public class MovingPlatform : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<Rigidbody2D>()) 
+        if (collision.GetComponent<Rigidbody2D>())
         {
             collision.transform.SetParent(transform);
         }
@@ -40,4 +42,16 @@ public class MovingPlatform : MonoBehaviour
     {
         collision.transform.SetParent(null);
     }
+
+    IEnumerator MoveToNextPosition()
+    {
+        while (Vector2.Distance(transform.position, movePositions[nextPos].position) > detectRad)
+        {
+            Vector2.Lerp(transform.position, movePositions[nextPos].position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        nextPos++;
+        if (nextPos >= movePositions.Length) nextPos = 0;
+    }
+
 }
