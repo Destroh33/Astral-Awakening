3240563e2ef92d78709be9ad8d4151ffa63869b1
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2025-07-24 14:26:47 -0700

    moving platform code

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] Transform[] movePositions;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] bool moveAutomatically;
+    [SerializeField] float detectRad;
+    int nextPos = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (moveAutomatically) 
+        {
+            Vector2.Lerp(transform.position, movePositions[nextPos].position, moveSpeed * Time.deltaTime);
+        }
+        if (Vector2.Distance(transform.position, movePositions[nextPos].position) < detectRad) 
+        {
+            nextPos++;
+            if (nextPos > movePositions.Length) nextPos = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>()) 
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
